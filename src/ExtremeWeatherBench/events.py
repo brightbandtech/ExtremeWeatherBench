4ab7d72a5c1f63ad92e02706926fb7f84cf25a6c
aaTman <mandelbaum.taylor@gmail.com>
2024-12-09 20:44:06 +0000

    starting to flesh out the dc's

--- src/ExtremeWeatherBench/events.py
+++ src/ExtremeWeatherBench/events.py
@@ -2,4 +2,45 @@ import yaml
 import xarray as xr
 import pandas as pd
 import numpy as np
+import dataclasses
+import ExtremeWeatherBench.utils as utils
+import typing as t
 
+@dataclasses.dataclass
+class Event:
+    """
+    Event holds the metadata that extends to all cases of a given event type.
+    """
+
+@dataclasses.dataclass
+class Case:
+    """
+    Case holds the event and climatology datasets for a given case. 
+    It also holds the metadata for the location and box length width in km. 
+    """
+    
+    case_analysis_ds: xr.Dataset
+    climatology_ds: xr.Dataset
+    location_center: dict
+    box_length_width_in_km: int
+
+    def __post_init__(self):
+        self.convert_longitude()
+        self.clip_and_remove_ocean()
+        self.align_climatology_to_event_analysis_time()
+        
+    def convert_longitude(self):
+        self.event_analysis_ds = utils.convert_longitude_to_180(self.event_analysis_ds)
+        self.climatology_ds = utils.convert_longitude_to_180(self.climatology_ds)
+
+    def clip_and_remove_ocean(self):
+        self.event_analysis_ds = self.process_dataset_to_spec(self.event_analysis_ds)
+        self.climatology_ds = self.process_dataset_to_spec(self.climatology_ds)
+    
+    def align_climatology_to_event_analysis_time(self):
+        self.climatology_ds = self.climatology_ds.sel(time=self.event_analysis_ds.time)
+    
+    def process_dataset_to_spec(self, xarray_data_object: t.Union[xr.Dataset,xr.DataArray]):
+        interim_xarray_data_object = utils.clip_dataset_to_square(xarray_data_object, self.location_center, self.box_length_width_in_km)
+        output_xarray_data_object = utils.remove_ocean_gridpoints(interim_xarray_data_object)
+        return output_xarray_data_object
