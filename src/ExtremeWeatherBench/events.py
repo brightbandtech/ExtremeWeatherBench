273b4bb23e23dcaf024190a0c714cf972b157925
aaTman <mandelbaum.taylor@gmail.com>
2024-12-09 21:16:11 +0000

    establish consistent vocabulary

--- src/ExtremeWeatherBench/events.py
+++ src/ExtremeWeatherBench/events.py
@@ -32,21 +32,22 @@ class Case(Event):
 
     def __post_init__(self):
         self.convert_longitude()
-        self.clip_and_remove_ocean()
-        self.align_climatology_to_event_analysis_time()
+        self.process_data_to_spec()
+        self.align_climatology_to_case_analysis_time()
+        #TODO: include obs data calls here from ISD
         
     def convert_longitude(self):
-        self.event_analysis_ds = utils.convert_longitude_to_180(self.event_analysis_ds)
+        self.case_analysis_ds = utils.convert_longitude_to_180(self.case_analysis_ds)
         self.climatology_ds = utils.convert_longitude_to_180(self.climatology_ds)
 
-    def clip_and_remove_ocean(self):
-        self.event_analysis_ds = self.process_dataset_to_spec(self.event_analysis_ds)
-        self.climatology_ds = self.process_dataset_to_spec(self.climatology_ds)
+    def process_data_to_spec(self):
+        self.case_analysis_ds = self.clip_and_remove_ocean(self.case_analysis_ds)
+        self.climatology_ds = self.clip_and_remove_ocean(self.climatology_ds)
     
-    def align_climatology_to_event_analysis_time(self):
-        self.climatology_ds = self.climatology_ds.sel(time=self.event_analysis_ds.time)
+    def align_climatology_to_case_analysis_time(self):
+        self.climatology_ds = self.climatology_ds.sel(time=self.case_analysis_ds.time)
     
-    def process_dataset_to_spec(self, xarray_data_object: t.Union[xr.Dataset,xr.DataArray]):
+    def clip_and_remove_ocean(self, xarray_data_object: t.Union[xr.Dataset,xr.DataArray]):
         interim_xarray_data_object = utils.clip_dataset_to_square(xarray_data_object, self.location_center, self.box_length_width_in_km)
         output_xarray_data_object = utils.remove_ocean_gridpoints(interim_xarray_data_object)
         return output_xarray_data_object
