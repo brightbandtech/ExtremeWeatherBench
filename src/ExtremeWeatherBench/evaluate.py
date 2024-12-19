23715a880d8ad1207dc5faa6a4ef754b4cf13c9f
Daniel Rothenberg <daniel@danielrothenberg.com>
2024-12-19 16:55:00 -0700

    Consolidate formatting, linting, code style, and on-going TODOs/NOTEs

--- src/ExtremeWeatherBench/evaluate.py
+++ src/ExtremeWeatherBench/evaluate.py
@@ -1,76 +1,106 @@
-'''Implements evaluation routines for the ExtremeWeatherBench library.'''
-import xarray as xr
-import pandas as pd
+"""Evaluation routines for use during ExtremeWeatherBench case studies / analyses."""
 
-#TODO: logging
-import logging
-import fsspec
-import numpy as np
+# TODO(taylor): Incorporate logging diagnostics throughout evaluation stack.
+# import logging
 import os
-from typing import Optional, List
+from typing import Optional
 
-#TODO: once uv/ruff/pyproject.toml is set up, remove relative imports
-from . import events
-from . import config
-from . import case
-from . import utils
-#: default configuration for mapping forecast dataset schema
+import pandas as pd
+import xarray as xr
+
+# TODO(taylor): once uv/ruff/pyproject.toml is set up, remove relative imports
+from . import case, config, events, utils
+
+#: Default mapping for forecast dataset schema.
 DEFAULT_FORECAST_SCHEMA_CONFIG = config.ForecastSchemaConfig()
 
-def evaluate(eval_config: config.Config, 
-             forecast_schema_config: config.ForecastSchemaConfig = DEFAULT_FORECAST_SCHEMA_CONFIG,
-             dry_run: bool = False) -> List[xr.Dataset]:
-    """ Evaluate the metrics, looping through each case for each event type.
-    Arguments:
-        eval_config: the configuration object for the evaluation
-        forecast_schema_config: the configuration object for the forecast variable naming schema
-        dry_run: a bool that is used to validate the cases without running the evaluation
+
+def evaluate(
+    eval_config: config.Config,
+    forecast_schema_config: config.ForecastSchemaConfig = DEFAULT_FORECAST_SCHEMA_CONFIG,
+    dry_run: bool = False,
+) -> dict[str, list[xr.Dataset]]
+    """Driver for evaluating a collection of Cases across a set of Events.
+
+    Args:
+        eval_config: A configuration object defining the evaluation run.
+        forecast_schema_config: A mapping of the forecast variable naming schema to use
+            when reading / decoding forecast data in the analysis.
+        dry_run: Flag to disable performing actual calculations (but still validate
+            case configurations). Defaults to "False."
+    
+    Returns:
+        A dictionary mapping event types to lists of xarray Datasets containing the
+        evaluation results for each case within the event type.
     """
 
-    point_obs, gridded_obs = utils._open_obs_datasets(eval_config) #TODO: more elegant design approach?
+    point_obs, gridded_obs = utils._open_obs_datasets(
+        eval_config
+    )  # TODO: more elegant design approach?
     forecast_dataset = utils._open_forecast_dataset(eval_config, forecast_schema_config)
 
-    #TODO: use importlib resources for this when uv/ruff/pyproject.toml is set up
+    # TODO: use importlib resources for this when uv/ruff/pyproject.toml is set up
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    events_file_path = os.path.join(base_dir, '../../assets/data/events.yaml')
+    events_file_path = os.path.join(base_dir, "../../assets/data/events.yaml")
+    all_results = {}
     for event in eval_config.event_types:
         cases = event.from_yaml(events_file_path)
         cases.build_metrics()
-        if dry_run: # temporary validation for the cases
+        if dry_run:  # temporary validation for the cases
             return cases
         else:
-            results = _evaluate_cases_loop(cases, forecast_dataset, gridded_obs, point_obs)
-    return results
+            results = _evaluate_cases_loop(
+                cases, forecast_dataset, gridded_obs, point_obs
+            )
+            # NOTE(daniel): This is a bit of a hack, but it's a quick way to get the
+            # event name for the dictionary key; can do something later, since we
+            # probably don't want to make Event objects hashable.
+            all_results[event.__name__] = results
+    return all_results
 
-def _evaluate_cases_loop(event: events.Event, 
-                         forecast_dataset: xr.Dataset,
-                         gridded_obs: Optional[xr.Dataset],
-                         point_obs: Optional[pd.DataFrame] 
-                         ) -> List[xr.Dataset]:
-    """ Loops through each case for each event type selected.
-    Arguments:
-        eval_config: the configuration object for the evaluation
-        forecast_schema_config: the configuration object for the forecast variable naming schema
-        dry_run: a bool that is used to validate the cases without running the evaluation
+
+def _evaluate_cases_loop(
+    event: events.Event,
+    forecast_dataset: xr.Dataset,
+    gridded_obs: Optional[xr.Dataset] = None,
+    point_obs: Optional[pd.DataFrame] = None,
+) -> list[xr.Dataset]:
+    """Sequentially loop over and evalute all cases for a specific event type.
+
+    Args:
+        event: The Event object containing the cases to evaluate.
+        forecast_dataset: The forecast dataset to evaluate against.
+        gridded_obs: The gridded observation dataset to use for evaluation.
+        point_obs: The point observation dataset to use for evaluation.
+
+    Returns:
+        A list of xarray Datasets containing the evaluation results for each case
+        in the Event of interest.
     """
     results = []
     for individual_case in event.cases:
-            results.append(_evaluate_case(individual_case, forecast_dataset, gridded_obs, point_obs))
+        results.append(
+            _evaluate_case(individual_case, forecast_dataset, gridded_obs, point_obs)
+        )
     return results
 
-def _evaluate_case(individual_case: case.IndividualCase, 
-                   forecast_dataset, 
-                   gridded_obs, 
-                   point_obs): #strongly type returns
-    """ Evaluate the forecast data against the observed data for a single case.
-    Arguments:
-        individual_case: the container object for one case within an event
-        forecast_dataset: the forecast xarray Dataset
-        gridded_obs: the gridded observation xarray Dataset
-        point_obs: the point observation pandas DataFrame
+
+def _evaluate_case(
+    individual_case: case.IndividualCase, forecast_dataset, gridded_obs, point_obs
+) -> xr.Dataset: 
+    """Evalaute a single case given forecast data and observations.
+
+    Args:
+        individual_case: A configuration object defining the case to evaluate.
+        forecast_dataset: The forecast dataset to evaluate against.
+        gridded_obs: The gridded observation dataset to use for evaluation.
+        point_obs: The point observation dataset to use for evaluation.
+
+    Returns:
+        An xarray Dataset containing the evaluation results for the case.
     """
-    #TODO: address incomplete functions
+    # TODO(taylor): Implement the actual evaluation logic here.
     if point_obs is not None:
-        pass 
+        pass
     if gridded_obs is not None:
         pass
--- src/ExtremeWeatherBench/forecast.py
+++ src/ExtremeWeatherBench/forecast.py
@@ -1,4 +1,5 @@
-import xarray as xr
 import typing as t
+
 import numpy as np
 import pandas as pd
+import xarray as xr
