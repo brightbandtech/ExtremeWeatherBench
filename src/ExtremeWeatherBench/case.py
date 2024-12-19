ba2a4e1eff73191950e002f2c8d0a946fe243d71
Daniel Rothenberg <daniel@danielrothenberg.com>
2024-12-19 16:55:00 -0700

    Consolidate formatting, linting, code style, and on-going TODOs/NOTEs

--- src/ExtremeWeatherBench/case.py
+++ src/ExtremeWeatherBench/case.py
@@ -1,21 +1,31 @@
-"""Contains the IndividualCase class for metadata of a single case."""
-import dataclasses
-from collections import namedtuple
-import datetime
+"""Utiltiies for defining individual units of case studies for analysis."""
 
-Location = namedtuple('Location', ['latitude', 'longitude'])
+import dataclasses
+import datetime
+from collections import namedtuple
+
+#: Struct packaging latitude/longitude location definitions.
+Location = namedtuple("Location", ["latitude", "longitude"])
+
 
 @dataclasses.dataclass
 class IndividualCase:
-    """
-    IndividualCase holds the metadata for a single case
-    based on the events.yaml metadata.
+    """Container for metadata defining a single or individual case.
+
+    An IndividualCase defines the relevant metadata for a single case study for a
+    given extreme weather event; it is designed to be easily instantiable through a
+    simple YAML-based configuration file.
+
     Attributes:
-        id: the numerical identifier for the event
-        start_date: datetime.date: the start date of the case
-        end_date: datetime.date: the end date of the case
-        location: Location: the latitude and longitude of the center of the location
-        bounding_box_km: int: the side length of the square in kilometers
+        id: A unique numerical identifier for the event.
+        start_date: A datetime.date object representing the start date of the case, for
+            use in subsetting data for analysis.
+        end_date: A datetime.date object representing the end date of the case, for use
+            in subsetting data for analysis.
+        location: A Location object representing the latitude and longitude of the event
+            center or  focus.
+        bounding_box_km: int: The side length of a bounding box centered on location, in
+            kilometers.
     """
 
     id: int
