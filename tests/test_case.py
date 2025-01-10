99815c8e93d749afac87b68f2fd5294eb1f10dba
aaTman <mandelbaum.taylor@gmail.com>
2025-01-10 04:15:50 +0000

    updates to testing

--- tests/test_case.py
+++ tests/test_case.py
@@ -8,46 +8,26 @@ import rioxarray
 from extremeweatherbench.utils import Location
 
 
-class TestCases:
+class TestGoodCases:
     @pytest.fixture
-    def mock_bad_dataset(self):
-        data = np.random.rand(20, 180, 360)
-        times = pd.date_range("2000-01-01", periods=20)
-        latitudes = np.linspace(-90, 90, 180)
-        longitudes = np.linspace(-180, 179, 360)
-        dataset = xr.Dataset(
-            {
-                "air_temperature": (["time", "latitude", "longitude"], data),
-                "eastward_wind": (["time", "latitude", "longitude"], data),
-                "northward_wind": (["time", "latitude", "longitude"], data),
-            },
-            coords={
-                "time": times,
-                "latitude": latitudes,
-                "longitude": longitudes,
-            },
-        )
-        return dataset
-
-    @pytest.fixture
-    def mock_good_dataset(self):
+    def mock_dataset(self):
         init_time = pd.date_range("2000-01-01", periods=5)
-        lead_time = range(0, 169, 6)
-        data = np.random.rand(5, 180, 360, len(lead_time))
+        time = range(0, 169, 6)
+        data = np.random.rand(5, 180, 360, len(time))
         latitudes = np.linspace(-90, 90, 180)
         longitudes = np.linspace(-180, 179, 360)
         dataset = xr.Dataset(
             {
                 "air_temperature": (
-                    ["init_time", "latitude", "longitude", "lead_time"],
+                    ["init_time", "latitude", "longitude", "time"],
                     data,
                 ),
                 "eastward_wind": (
-                    ["init_time", "latitude", "longitude", "lead_time"],
+                    ["init_time", "latitude", "longitude", "time"],
                     data,
                 ),
                 "northward_wind": (
-                    ["init_time", "latitude", "longitude", "lead_time"],
+                    ["init_time", "latitude", "longitude", "time"],
                     data,
                 ),
             },
@@ -55,16 +35,24 @@ class TestCases:
                 "init_time": init_time,
                 "latitude": latitudes,
                 "longitude": longitudes,
-                "lead_time": lead_time,
+                "time": time,
             },
         )
         return dataset
 
-    @pytest.fixture
-    def mock_datasets(self, mock_bad_dataset, mock_good_dataset):
-        return [self.mock_bad_dataset, self.mock_good_dataset]
+    def test_check_for_forecast_data_availability(self, mock_dataset):
+        base_case = case.IndividualCase(
+            id=10,
+            title="Test Case",
+            start_date=datetime.date(2000, 1, 1),
+            end_date=datetime.date(2000, 1, 10),
+            location={"latitude": 40, "longitude": -100},
+            bounding_box_km=500,
+            event_type="heat_wave",
+        )
+        assert base_case._check_for_forecast_data_availability(mock_dataset) is True
 
-    def test_perform_subsetting_procedure_heatwave(self, dataset):
+    def test_perform_subsetting_procedure_heatwave(self, mock_dataset):
         heatwave_case = case.IndividualHeatWaveCase(
             id=20,
             title="Test Heatwave",
@@ -74,12 +62,12 @@ class TestCases:
             bounding_box_km=500,
             event_type="heat_wave",
         )
-        subset_dataset = heatwave_case.perform_subsetting_procedure(dataset())
+        subset_dataset = heatwave_case.perform_subsetting_procedure(mock_dataset)
         assert "air_temperature" in subset_dataset
         assert "eastward_wind" not in subset_dataset
         assert "northward_wind" not in subset_dataset
 
-    def test_perform_subsetting_procedure_freeze(self, dataset):
+    def test_perform_subsetting_procedure_freeze(self, mock_dataset):
         freeze_case = case.IndividualFreezeCase(
             id=10,
             title="Test Freeze",
@@ -89,13 +77,12 @@ class TestCases:
             bounding_box_km=500,
             event_type="freeze",
         )
-        subset_dataset = freeze_case.perform_subsetting_procedure(dataset())
+        subset_dataset = freeze_case.perform_subsetting_procedure(mock_dataset)
         assert "air_temperature" in subset_dataset
         assert "eastward_wind" in subset_dataset
         assert "northward_wind" in subset_dataset
 
-    @pytest.mark.parametrize("dataset", [mock_bad_dataset, mock_good_dataset])
-    def test_individual_case(self, dataset):
+    def test_individual_case(self, mock_dataset):
         base_case = case.IndividualCase(
             id=10,
             title="Test Case",
@@ -117,8 +104,8 @@ class TestCases:
             "data_vars": None,
         }
         assert base_case.__dict__ == valid_case
-        assert base_case._subset_data_vars(dataset) is dataset
-        assert base_case._subset_valid_times(dataset).time.shape[0] > 1
+        assert base_case._subset_data_vars(mock_dataset) is mock_dataset
+        assert base_case._subset_valid_times(mock_dataset).time.shape[0] > 1
         with pytest.raises(NotImplementedError):
-            base_case.perform_subsetting_procedure(dataset)
-        assert base_case._check_for_forecast_data_availability(dataset) is True
+            base_case.perform_subsetting_procedure(mock_dataset)
+        assert base_case._check_for_forecast_data_availability(mock_dataset) is True
