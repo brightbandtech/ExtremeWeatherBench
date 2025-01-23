697064eec72cbc42ec36d0b07b2690f196569038
taylor <mandelbaum.taylor@gmail.com>
2025-01-22 23:10:34 -0500

    100% metric coverage, slight refactors to reduce repeated func calls

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -10,14 +10,43 @@ def dataset_to_dataarray(dataset):
     return dataset[mock_data_var]
 
 
+class TestMetric:
+    def test_name_property(self):
+        metric = metrics.Metric()
+        assert metric.name == "Metric"
+
+    def test_compute_not_implemented(
+        self, mock_forecast_dataarray, mock_gridded_obs_dataarray
+    ):
+        metric = metrics.Metric()
+        with pytest.raises(NotImplementedError):
+            metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
+
+    def test_align_datasets(self, mock_forecast_dataarray, mock_gridded_obs_dataarray):
+        metric = metrics.Metric()
+        init_time_datetime = pd.Timestamp(
+            mock_forecast_dataarray.init_time[0].values
+        ).to_pydatetime()
+        aligned_forecast, aligned_obs = metric.align_datasets(
+            mock_forecast_dataarray, mock_gridded_obs_dataarray, init_time_datetime
+        )
+
+        # Check aligned datasets have same time coordinates
+        assert (aligned_forecast.time == aligned_obs.time).all()
+
+        # Check forecast was properly subset by init time
+        assert aligned_forecast.init_time.size == 1
+        assert pd.Timestamp(aligned_forecast.init_time.values) == pd.Timestamp(
+            mock_forecast_dataarray.init_time[0].values
+        )
+
+
 class TestRegionalRMSE:
     def test_regional_rmse_compute(
-        self, mock_forecast_dataset, mock_gridded_obs_dataset
+        self, mock_forecast_dataarray, mock_gridded_obs_dataarray
     ):
         # Instantiate the RegionalRMSE metric
         metric = metrics.RegionalRMSE()
-        mock_forecast_dataarray = dataset_to_dataarray(mock_forecast_dataset)
-        mock_gridded_obs_dataarray = dataset_to_dataarray(mock_gridded_obs_dataset)
         # Compute the RMSE
         result = metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
 
@@ -28,12 +57,9 @@ class TestRegionalRMSE:
 
 
 class TestMaximumMAE:
-    def test_base_compute(self, mock_forecast_dataset, mock_gridded_obs_dataset):
+    def test_base_compute(self, mock_forecast_dataarray, mock_gridded_obs_dataarray):
         # Instantiate the MaximumMAE metric
         metric = metrics.MaximumMAE()
-        # Turn fixtures into dataarrays
-        mock_forecast_dataarray = dataset_to_dataarray(mock_forecast_dataset)
-        mock_gridded_obs_dataarray = dataset_to_dataarray(mock_gridded_obs_dataset)
         # Compute the RMSE
 
         result = metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
@@ -43,16 +69,10 @@ class TestMaximumMAE:
         assert "lead_time" in result.dims
 
     def test_in_forecast_dataarray(
-        self, mock_forecast_dataset, mock_gridded_obs_dataset_max_in_forecast
+        self, mock_forecast_dataarray, mock_gridded_obs_dataarray_max_in_forecast
     ):
         # Instantiate the MaximumMAE metric
         metric = metrics.MaximumMAE()
-
-        # Turn fixtures into dataarrays
-        mock_forecast_dataarray = dataset_to_dataarray(mock_forecast_dataset)
-        mock_gridded_obs_dataarray_max_in_forecast = dataset_to_dataarray(
-            mock_gridded_obs_dataset_max_in_forecast
-        )
         result = metric.compute(
             mock_forecast_dataarray, mock_gridded_obs_dataarray_max_in_forecast
         )
@@ -68,12 +88,10 @@ class TestMaximumMAE:
 
 class TestMaxOfMinTempMAE:
     def test_max_of_min_temp_mae_compute(
-        self, mock_forecast_dataset, mock_gridded_obs_dataset
+        self, mock_forecast_dataarray, mock_gridded_obs_dataarray
     ):
         # Instantiate the MaxOfMinTempMAE metric
         metric = metrics.MaxOfMinTempMAE()
-        mock_forecast_dataarray = dataset_to_dataarray(mock_forecast_dataset)
-        mock_gridded_obs_dataarray = dataset_to_dataarray(mock_gridded_obs_dataset)
         # Compute the RMSE
         result = metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
         # Check the result is an xarray DataArray
@@ -81,29 +99,23 @@ class TestMaxOfMinTempMAE:
         # Check the dimensions of the result
         assert "lead_time" in result.dims
 
-        def test_metric_base_class():
-            # Test base Metric class name property
-            metric = metrics.Metric()
-            assert metric.name == "Metric"
-
-            # Test that compute raises NotImplementedError
-            mock_forecast = xr.DataArray()
-            mock_obs = xr.DataArray()
-            with pytest.raises(NotImplementedError):
-                metric.compute(mock_forecast, mock_obs)
+    def test_max_of_min_temp_mae_wrong_data_var(
+        self, mock_forecast_dataarray, mock_gridded_obs_dataarray
+    ):
+        mock_forecast_dataarray = mock_forecast_dataarray.rename("bad_name")
+        metric = metrics.MaxOfMinTempMAE()
+        with pytest.raises(KeyError):
+            metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
 
 
-def test_metric_align_datasets(mock_forecast_dataset, mock_gridded_obs_dataset):
+def test_metric_align_datasets(mock_forecast_dataarray, mock_gridded_obs_dataarray):
     # Test dataset alignment method
     metric = metrics.Metric()
-    mock_forecast = dataset_to_dataarray(mock_forecast_dataset)
-    mock_obs = dataset_to_dataarray(mock_gridded_obs_dataset)
-
-    init_time = mock_forecast.init_time[0].values
+    init_time = mock_forecast_dataarray.init_time[0].values
     init_time_dt = pd.Timestamp(init_time).to_pydatetime()
 
     aligned_forecast, aligned_obs = metric.align_datasets(
-        mock_forecast, mock_obs, init_time_dt
+        mock_forecast_dataarray, mock_gridded_obs_dataarray, init_time_dt
     )
 
     # Check aligned datasets have same time coordinates
@@ -112,3 +124,21 @@ def test_metric_align_datasets(mock_forecast_dataset, mock_gridded_obs_dataset):
     # Check forecast was properly subset by init time
     assert aligned_forecast.init_time.size == 1
     assert pd.Timestamp(aligned_forecast.init_time.values) == pd.Timestamp(init_time)
+
+
+class TestOnsetME:
+    def test_compute(self, mock_forecast_dataarray, mock_gridded_obs_dataarray):
+        metric = metrics.OnsetME()
+        with pytest.raises(
+            NotImplementedError, match="Onset mean error not yet implemented."
+        ):
+            metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
+
+
+class TestDurationME:
+    def test_compute(self, mock_forecast_dataarray, mock_gridded_obs_dataarray):
+        metric = metrics.DurationME()
+        with pytest.raises(
+            NotImplementedError, match="Duration mean error not yet implemented."
+        ):
+            metric.compute(mock_forecast_dataarray, mock_gridded_obs_dataarray)
