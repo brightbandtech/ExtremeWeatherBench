584ca74f7cb34fe77c08c0f6090318e1bc564bed
taylor <mandelbaum.taylor@gmail.com>
2025-02-03 18:43:58 -0500

    add parents

--- src/extremeweatherbench/evaluate.py
+++ src/extremeweatherbench/evaluate.py
@@ -36,7 +36,7 @@ def evaluate(
         A dictionary mapping event types to lists of xarray Datasets containing the
         evaluation results for each case within the event type.
     """
-    base_dir = Path(__file__).resolve()
+    base_dir = Path(__file__).resolve().parents[1]
     events_file_path = base_dir / "assets" / "data" / "events.yaml"
     all_results = {}
     with open(events_file_path, "r") as file:
