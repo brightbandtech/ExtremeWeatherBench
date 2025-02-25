cb4f8b104c8c46575a1fdc377dd15e7e74c58390
aaTman <mandelbaum.taylor@gmail.com>
2025-02-25 02:04:57 +0000

    clean up utils

--- src/extremeweatherbench/utils.py
+++ src/extremeweatherbench/utils.py
@@ -4,7 +4,6 @@ other specialized package.
 
 from typing import Union, List, Tuple
 from collections import namedtuple
-import fsspec
 import numpy as np
 import pandas as pd
 import regionmask
@@ -167,100 +166,6 @@ def remove_ocean_gridpoints(dataset: xr.Dataset) -> xr.Dataset:
     dataset = dataset.where(land_mask, drop=True)
 
     return dataset
-
-
-def _open_kerchunk_zarr_reference_jsons(
-    file_list, forecast_schema_config, remote_protocol: str = "s3"
-):
-    """Open a dataset from a list of kerchunk JSON files."""
-    xarray_datasets = []
-    for json_file in file_list:
-        fs_ = fsspec.filesystem(
-            "reference",
-            fo=json_file,
-            ref_storage_args={"skip_instance_cache": True},
-            remote_protocol=remote_protocol,
-            remote_options={"anon": True},
-        )
-        m = fs_.get_mapper("")
-        ds = xr.open_dataset(
-            m, engine="zarr", backend_kwargs={"consolidated": False}, chunks="auto"
-        )
-        if "initialization_time" not in ds.attrs:
-            raise ValueError(
-                "Initialization time not found in dataset attributes. \
-                             Please add initialization_time to the dataset attributes."
-            )
-        else:
-            model_run_time = np.datetime64(
-                pd.to_datetime(ds.attrs["initialization_time"])
-            )
-        ds[forecast_schema_config.init_time] = model_run_time.astype("datetime64[ns]")
-        ds = ds.set_coords(forecast_schema_config.init_time)
-        ds = ds.expand_dims(forecast_schema_config.init_time)
-        for variable in forecast_schema_config.__dict__:
-            attr_value = getattr(forecast_schema_config, variable)
-            if attr_value in ds.data_vars:
-                ds = ds.rename({attr_value: variable})
-        ds = ds.transpose(
-            forecast_schema_config.init_time,
-            forecast_schema_config.valid_time,
-            forecast_schema_config.latitude,
-            forecast_schema_config.longitude,
-            forecast_schema_config.level,
-        )
-        xarray_datasets.append(ds)
-
-    return xr.concat(xarray_datasets, dim=forecast_schema_config.init_time)
-
-
-def _open_mlwp_kerchunk_reference(
-    file, forecast_schema_config, remote_protocol: str = "s3"
-):
-    """Open a dataset from a kerchunked reference file for the OAR MLWP S3 bucket."""
-    if "parq" in file:
-        storage_options = {
-            "remote_protocol": "s3",
-            "skip_instance_cache": True,
-            "remote_options": {"anon": True},
-            "target_protocol": "file",
-            "lazy": True,
-        }  # options passed to fsspec
-        open_dataset_options: dict = {"chunks": {}}  # opens passed to xarray
-
-        ds = xr.open_dataset(
-            file,
-            engine="kerchunk",
-            storage_options=storage_options,
-            open_dataset_options=open_dataset_options,
-        )
-    else:
-        ds = xr.open_dataset(
-            "reference://",
-            engine="zarr",
-            backend_kwargs={
-                "consolidated": False,
-                "storage_options": {
-                    "fo": file,
-                    "remote_protocol": remote_protocol,
-                    "remote_options": {"anon": True},
-                },
-            },
-        )
-    ds = ds.rename({"time": "lead_time"})
-    ds["lead_time"] = range(0, 241, 6)
-    for variable in forecast_schema_config.__dict__:
-        attr_value = getattr(forecast_schema_config, variable)
-        if attr_value in ds.data_vars:
-            ds = ds.rename({attr_value: variable})
-        # Step 1: Create a meshgrid of init_time and lead_time
-    lead_time_grid, init_time_grid = np.meshgrid(ds.lead_time, ds.init_time)
-    # Step 2: Flatten the meshgrid and convert lead_time to timedelta
-    valid_time = init_time_grid.flatten() + pd.to_timedelta(
-        lead_time_grid.flatten(), unit="h"
-    )
-    ds.coords["time"] = valid_time
-    return ds
 
 
 def map_era5_vars_to_forecast(forecast_schema_config, forecast_dataset, era5_dataset):
