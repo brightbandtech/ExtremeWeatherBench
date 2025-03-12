9e893ff95cb249f7f7a6809840021979ead092d2
aaTman <mandelbaum.taylor@gmail.com>
2025-03-12 16:45:43 +0000

    revert metric dict work for other pr

--- src/extremeweatherbench/evaluate.py
+++ src/extremeweatherbench/evaluate.py
@@ -392,30 +392,24 @@ def _maybe_evaluate_individual_case(
         observation=point_obs,
         forecast=forecast_dataset,
     )
+
     gridded_case_eval = build_dataarray_subsets(gridded_obs_evaluation, compute=True)
     point_case_eval = build_dataarray_subsets(point_obs_evaluation, compute=True)
-    for data_var, eval, metric in itertools.product(
-        individual_case.data_vars,
-        [gridded_case_eval, point_case_eval],
-        individual_case.metrics_list,
+    for data_var, metric in itertools.product(
+        individual_case.data_vars, individual_case.metrics_list
     ):
-        if data_var not in case_results:
-            case_results[data_var] = {}
-        if eval.observation_type not in case_results[data_var]:
-            case_results[data_var][eval.observation_type] = {}
-
+        case_results[data_var] = {}
         metric_instance = metric()
-
+        logging.debug("metric %s computing", metric_instance.name)
         # TODO(aaTman): Create metric container object for gridded and point obs
         # in the meantime, forcing a check for Nones in gridded and point eval objects
-
-        if eval.observation is not None and eval.forecast is not None:
-            logging.info("metric %s computing", metric_instance.name)
-            case_results[data_var][eval.observation_type][metric_instance.name] = (
-                metric_instance.compute(
+        result = {}
+        for eval in [gridded_case_eval, point_case_eval]:
+            if eval.observation is not None and eval.forecast is not None:
+                result[eval.observation_type] = metric_instance.compute(
                     eval.forecast[data_var], eval.observation[data_var]
                 )
-            )
-        else:
-            case_results[data_var][eval.observation_type][metric_instance.name] = None
+            else:
+                result[eval.observation_type] = None
+        case_results[data_var][metric_instance.name] = result
     return case_results
