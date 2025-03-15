7edb4e93a5af00194f6130b08a52e7cb2a1cbeb0
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2025-03-15 12:47:47 -0400

    suggested updates from PR comments

--- src/extremeweatherbench/data_loader.py
+++ src/extremeweatherbench/data_loader.py
@@ -1,4 +1,3 @@
-import fsspec
 import xarray as xr
 from extremeweatherbench import config, utils
 import pandas as pd
@@ -16,6 +15,9 @@ def open_forecast_dataset(
 ) -> xr.Dataset:
     """Open the forecast dataset specified for evaluation.
 
+    If a URI is provided (e.g. s3://bucket/path/to/forecast), the filesystem
+    will be inferred from the provided source (in this case, s3). Otherwise,
+    the filesystem will assumed to be local.
     Args:
         eval_config: The evaluation configuration.
         forecast_schema_config: The forecast schema configuration.
@@ -24,112 +26,86 @@ def open_forecast_dataset(
         The opened forecast dataset.
     """
     logger.debug("Opening forecast dataset")
-    if "://" in eval_config.forecast_dir:
-        filesystem = eval_config.forecast_dir.split("://")[0]
+    if "zarr" in eval_config.forecast_dir:
+        forecast_ds = xr.open_zarr(eval_config.forecast_dir, chunks="auto")
+    elif (
+        "parq" in eval_config.forecast_dir
+        or "json" in eval_config.forecast_dir
+        or "parquet" in eval_config.forecast_dir
+    ):
+        forecast_ds = open_kerchunk_reference(eval_config)
     else:
-        filesystem = "file"
-    fs = fsspec.filesystem(filesystem)
-    file_list = fs.ls(eval_config.forecast_dir)
-    file_types = set([file.split(".")[-1] for file in file_list])
-
-    if len(file_types) > 1:
-        if (
-            "parq" not in eval_config.forecast_dir
-            and "zarr" not in eval_config.forecast_dir
-        ):
-            raise TypeError("Multiple file types found in forecast path.")
-        if "parq" in eval_config.forecast_dir:
-            # kerchunked parq refs seem to consistently need two "compute" calls
-            # the first one doesn't actually load to memory
-            forecast_ds = open_kerchunk_reference(
-                eval_config.forecast_dir, forecast_schema_config
-            )
-        elif "zarr" in eval_config.forecast_dir:
-            forecast_ds = xr.open_zarr(eval_config.forecast_dir, chunks="auto")
-    elif len(file_types) == 1:
-        if "zarr" in file_types:
-            forecast_ds = xr.open_zarr(file_list, chunks="auto")
-        elif "json" in file_types:
-            forecast_ds = open_kerchunk_reference(file_list[0], forecast_schema_config)
-        else:
-            raise TypeError(
-                "Unknown file type found in forecast path, only json, parquet, and zarr are supported."
-            )
-    else:
-        raise FileNotFoundError("No files found in forecast path.")
-
-    mapping = {
-        getattr(forecast_schema_config, field.name): field.name
-        for field in dataclasses.fields(forecast_schema_config)
-    }
-
-    filtered_mapping_data_vars = {
-        old: new for old, new in mapping.items() if old in forecast_ds.data_vars
-    }
-    filtered_mapping_coords = {
-        old: new for old, new in mapping.items() if old in forecast_ds.coords
-    }
-    filtered_mapping = {**filtered_mapping_data_vars, **filtered_mapping_coords}
-    forecast_ds = forecast_ds.rename(filtered_mapping)
-    forecast_ds = utils.convert_dataset_lead_time_to_int(forecast_ds)
+        raise TypeError(
+            "Unknown file type found in forecast path, only json, parquet, and zarr are supported."
+        )
+    forecast_ds = _rename_forecast_dataset(forecast_ds, forecast_schema_config)
+    forecast_ds = utils.maybe_convert_dataset_lead_time_to_int(forecast_ds)
 
     return forecast_ds
 
 
 def open_kerchunk_reference(
-    file,
-    forecast_schema_config: config.ForecastSchemaConfig,
-    remote_protocol: str = "s3",
+    eval_config: config.Config,
 ) -> xr.Dataset:
     """Open a dataset from a kerchunked reference file in parquet or json format.
-    This has been built for the CIRA MLWP S3 bucket's data, but can work with other
-    data as well if it is 6 hourly.
+    This has been built for the CIRA MLWP S3 bucket's data (https://registry.opendata.aws/aiwp/),
+    but can work with other data in the future. Currently only supports CIRA data unless
+    schema is identical to the CIRA schema.
 
     Args:
         file: The path to the kerchunked reference file.
-        forecast_schema_config: The forecast schema configuration.
         remote_protocol: The remote protocol to use.
 
     Returns:
         The opened dataset.
     """
-    if "parq" in file:
+    if "parq" in eval_config.forecast_dir or "parquet" in eval_config.forecast_dir:
         storage_options = {
-            "remote_protocol": remote_protocol,
+            "remote_protocol": eval_config.remote_protocol,
             "remote_options": {"anon": True},
         }  # options passed to fsspec
         open_dataset_options: dict = {"chunks": {}}  # opens passed to xarray
 
         kerchunk_ds = xr.open_dataset(
-            file,
+            eval_config.forecast_dir,
             engine="kerchunk",
             storage_options=storage_options,
             open_dataset_options=open_dataset_options,
         )
         kerchunk_ds = kerchunk_ds.compute()
-    elif "json" in file:
+    elif "json" in eval_config.forecast_dir:
         kerchunk_ds = xr.open_dataset(
             "reference://",
             engine="zarr",
             backend_kwargs={
                 "consolidated": False,
                 "storage_options": {
-                    "fo": file,
-                    "remote_protocol": remote_protocol,
+                    "fo": eval_config.forecast_dir,
+                    "remote_protocol": eval_config.remote_protocol,
                     "remote_options": {"anon": True},
                 },
             },
         )
     else:
         raise TypeError(
-            "Unknown file type found in forecast path, only json and parquet are supported."
+            "Unknown kerchunk file type found in forecast path, only json and parquet are supported."
         )
-    kerchunk_ds = kerchunk_ds.rename({"time": "lead_time"})
-    kerchunk_ds["lead_time"] = range(0, 241, 6)
-    for variable in forecast_schema_config.__dict__:
-        attr_value = getattr(forecast_schema_config, variable)
-        if attr_value in kerchunk_ds.data_vars:
-            kerchunk_ds = kerchunk_ds.rename({attr_value: variable})
+    if eval_config.forecast_source == "cira":
+        kerchunk_ds = kerchunk_ds.rename({"time": "lead_time"})
+
+        # The evaluation configuration is used to set the lead time range and resolution.
+        kerchunk_ds["lead_time"] = range(
+            eval_config.timestep_begin,
+            (
+                eval_config.timestep_begin
+                + eval_config.output_timesteps * eval_config.temporal_resolution_hours
+            ),
+            eval_config.temporal_resolution_hours,
+        )
+    else:
+        raise NotImplementedError(
+            "Only CIRA data is currently supported for kerchunked reference files."
+        )
     return kerchunk_ds
 
 
@@ -156,6 +132,38 @@ def open_obs_datasets(
             chunks=None,
             storage_options=dict(token="anon"),
         )
-    if point_obs is None and gridded_obs is None:
-        raise FileNotFoundError("No gridded or point observation data provided.")
     return point_obs, gridded_obs
+
+
+def _rename_forecast_dataset(
+    forecast_ds: xr.Dataset, forecast_schema_config: config.ForecastSchemaConfig
+) -> xr.Dataset:
+    """Rename the forecast dataset to the correct names expected by the evaluation routines.
+
+    Args:
+        forecast_ds: The forecast dataset to rename.
+        forecast_schema_config: The forecast schema configuration.
+
+    Returns:
+        The renamed forecast dataset.
+    """
+    # Mapping here is used to rename the incoming data variables to the correct
+    # names expected by the evaluation routines.
+    mapping = {
+        getattr(forecast_schema_config, field.name): field.name
+        for field in dataclasses.fields(forecast_schema_config)
+    }
+
+    # Application of the mapping to coords and data variables.
+    filtered_mapping_data_vars = {
+        old: new for old, new in mapping.items() if old in forecast_ds.data_vars
+    }
+    filtered_mapping_coords = {
+        old: new for old, new in mapping.items() if old in forecast_ds.coords
+    }
+
+    # Combine the mapping for coords and data variables and rename them in the forecast dataset.
+    filtered_mapping = {**filtered_mapping_data_vars, **filtered_mapping_coords}
+    forecast_ds = forecast_ds.rename(filtered_mapping)
+
+    return forecast_ds
