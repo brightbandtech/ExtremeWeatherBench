6347991d1348a0bd8201b2a2011c49fef34ded50
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2025-07-17 14:36:21 -0400

    change all instances of id to case_id_number (#98)

--- src/extremeweatherbench/case.py
+++ src/extremeweatherbench/case.py
@@ -3,12 +3,14 @@ Some code similarly structured to WeatherBench (Rasp et al.)."""
 
 import dataclasses
 import datetime
-from typing import List, Optional, Type
-from extremeweatherbench import metrics, utils
-import xarray as xr
-from enum import StrEnum
-import numpy as np
 import logging
+from enum import StrEnum
+from typing import List, Optional, Type
+
+import numpy as np
+import xarray as xr
+
+from extremeweatherbench import metrics, utils
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -23,7 +25,7 @@ class IndividualCase:
     simple YAML-based configuration file.
 
     Attributes:
-        id: A unique numerical identifier for the event.
+        case_id_number: A unique numerical identifier for the event.
         start_date: The start date of the case, for use in subsetting data for analysis.
         end_date: The end date of the case, for use in subsetting data for analysis.
         location: A Location object representing the latitude and longitude of the event
@@ -34,7 +36,7 @@ class IndividualCase:
         cross_listed: A list of other event types that this case study is cross-listed under.
     """
 
-    id: int
+    case_id_number: int
     title: str
     start_date: datetime.datetime
     end_date: datetime.datetime
@@ -102,16 +104,20 @@ class IndividualCase:
         lead_time_len = len(forecast_dataset.init_time)
 
         if lead_time_len == 0:
-            logger.warning("No forecast data available for case %s, skipping", self.id)
+            logger.warning(
+                "No forecast data available for case %s, skipping", self.case_id_number
+            )
             return False
         elif lead_time_len < (self.end_date - self.start_date).days:
             logger.warning(
                 "Fewer valid times in forecast than days in case %s, results likely unreliable",
-                self.id,
+                self.case_id_number,
             )
         else:
-            logger.info("Forecast data available for case %s", self.id)
-        logger.info("Lead time length for case %s: %s", self.id, lead_time_len)
+            logger.info("Forecast data available for case %s", self.case_id_number)
+        logger.info(
+            "Lead time length for case %s: %s", self.case_id_number, lead_time_len
+        )
         return True
 
 
