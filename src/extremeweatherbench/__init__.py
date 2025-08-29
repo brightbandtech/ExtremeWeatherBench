05ce41475b4743c45eadbfb80deafe0ac0a38671
taylor <mandelbaum.taylor@gmail.com>
2025-08-29 00:40:31 +0000

    add progress trackers to init

---
+++ src/extremeweatherbench/__init__.py
@@ -0,0 +1,11 @@
+from extremeweatherbench.progress import (
+    EWBProgressTracker,
+    enhanced_logging,
+    progress_tracker,
+)
+
+__all__ = [
+    "EWBProgressTracker",
+    "enhanced_logging",
+    "progress_tracker",
+]
