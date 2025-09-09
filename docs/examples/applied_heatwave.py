b29fee0b8601880ca7e76cafaee7e084264379ec
taylor <mandelbaum.taylor@gmail.com>
2025-09-09 19:58:23 +0000

    update heatwave scripts

--- docs/examples/applied_heatwave.py
+++ docs/examples/applied_heatwave.py
@@ -5,15 +5,17 @@ import xarray as xr
 
 from extremeweatherbench import evaluate, inputs, metrics, utils
 
+# Suppress noisy log messages
 logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
 logging.getLogger("botocore.httpchecksum").setLevel(logging.CRITICAL)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Load events yaml
 case_yaml = utils.load_events_yaml()
-test_yaml = {"cases": case_yaml["cases"][:1]}
 
 
+# Preprocess function for CIRA data using Brightband kerchunk parquets
 def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
     """An example preprocess function that renames the time coordinate to lead_time,
     creates a valid_time coordinate, and sets the lead time range and resolution not
@@ -33,6 +35,8 @@ def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
     return ds
 
 
+# Define targets
+# ERA5 target
 era5_heatwave_target = inputs.ERA5(
     source=inputs.ARCO_ERA5_FULL_URI,
     variables=["surface_air_temperature"],
@@ -44,13 +48,13 @@ era5_heatwave_target = inputs.ERA5(
     chunks=None,
 )
 
+# GHCN target
 ghcn_target = inputs.GHCN(
     source=inputs.DEFAULT_GHCN_URI,
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
-    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
 )
 
+# Define forecast (HRES)
 hres_forecast = inputs.ZarrForecast(
     source="gs://weatherbench2/datasets/hres/2016-2022-0012-1440x721.zarr",
     variables=["surface_air_temperature"],
@@ -62,8 +66,8 @@ hres_forecast = inputs.ZarrForecast(
     storage_options={"remote_options": {"anon": True}},
 )
 
-# just one for now
-heatwave_metric_list = [
+# Example of one evaluation object for heatwave
+heatwave_evaluation_object = [
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=[
@@ -73,28 +77,18 @@ heatwave_metric_list = [
             metrics.DurationME,
             metrics.MaxMinMAE,
         ],
-        target=era5_heatwave_target,
+        target=ghcn_target,
         forecast=hres_forecast,
-    ),
-    # rs.EvaluationObject(
-    #     event_type="heat_wave",
-    #     metric_list=[
-    #         crs.MaximumMAE,
-    #         crs.RMSE,
-    #         crs.OnsetME,
-    #         crs.DurationME,
-    #         crs.MaxMinMAE,
-    #     ],
-    #     target_config=ghcn_target_config,
-    #     forecast_config=cira_forecast_config,
-    # ),
+    )
 ]
 
+# Initialize ExtremeWeatherBench
 test_ewb = evaluate.ExtremeWeatherBench(
-    cases=test_yaml,
-    metrics=heatwave_metric_list,
+    cases=case_yaml,
+    evaluation_objects=heatwave_evaluation_object,
 )
-logger.info("Starting EWB run")
+
+# Run the workflow
 outputs = test_ewb.run(
     # tolerance range is the number of hours before and after the timestamp a
     # validating occurrence is checked in the forecasts
@@ -103,4 +97,5 @@ outputs = test_ewb.run(
     pre_compute=True,
 )
 
+# Print the outputs; can be saved if desired
 print(outputs.head())
--- docs/examples/applied_heatwave_parallel.py
+++ docs/examples/applied_heatwave_parallel.py
@@ -8,9 +8,7 @@ import xarray as xr
 from tqdm.auto import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
-from extremeweatherbench import config, evaluate, inputs, metrics, utils
-
-log = logging.getLogger(__name__)
+from extremeweatherbench import evaluate, inputs, metrics, utils
 
 
 def configure_root_logger():
@@ -28,28 +26,17 @@ def configure_root_logger():
     return root
 
 
-# def configure_worker_logger(log_name="Default", log_level="DEBUG"):
-#     def decorator(func: callable):
-#         def wrapper(*args, **kwargs):
-#             worker_logger = logging.getLogger(log_name)
-#             if not worker_logger.hasHandlers():
-#                 h = QueueHandler(kwargs["log_queue"])
-#                 worker_logger.addHandler(h)
-#             worker_logger.setLevel(log_level)
-#             kwargs["log_queue"] = worker_logger
-#             return func(*args, **kwargs)
-
-#         return wrapper
-
-
+# Suppress noisy log messages
+logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
+logging.getLogger("botocore.httpchecksum").setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-case_yaml = utils.read_event_yaml(
-    "/home/taylor/ExtremeWeatherBench/src/extremeweatherbench/data/events.yaml"
-)
-test_yaml = {"cases": case_yaml["cases"][:]}
+# Load events yaml
+case_yaml = utils.load_events_yaml()
 
 
+# Preprocess function for CIRA data using Brightband kerchunk parquets
 def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
     """An example preprocess function that renames the time coordinate to lead_time,
     creates a valid_time coordinate, and sets the lead time range and resolution not
@@ -69,8 +56,9 @@ def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
     return ds
 
 
-era5_target_config = config.TargetConfig(
-    target=inputs.ERA5,
+# Define targets
+# ERA5 target
+era5_heatwave_target = inputs.ERA5(
     source=inputs.ARCO_ERA5_FULL_URI,
     variables=["surface_air_temperature"],
     variable_mapping={
@@ -78,26 +66,30 @@ era5_target_config = config.TargetConfig(
         "time": "valid_time",
     },
     storage_options={"remote_options": {"anon": True}},
-)
-ghcn_target_config = config.TargetConfig(
-    target=inputs.GHCN,
-    source=inputs.DEFAULT_GHCN_URI,
-    variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
-    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
-)
-cira_forecast_config = config.ForecastConfig(
-    forecast=inputs.KerchunkForecast,
-    source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
-    variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
-    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
-    preprocess=_preprocess_bb_cira_forecast_dataset,
+    chunks=None,
 )
 
-# just one for now
-heatwave_metric_list = [
-    config.MetricEvaluationObject(
+# GHCN target
+ghcn_target = inputs.GHCN(
+    source=inputs.DEFAULT_GHCN_URI,
+    variables=["surface_air_temperature"],
+)
+
+# Define forecast (HRES)
+hres_forecast = inputs.ZarrForecast(
+    source="gs://weatherbench2/datasets/hres/2016-2022-0012-1440x721.zarr",
+    variables=["surface_air_temperature"],
+    variable_mapping={
+        "2m_temperature": "surface_air_temperature",
+        "prediction_timedelta": "lead_time",
+        "time": "init_time",
+    },
+    storage_options={"remote_options": {"anon": True}},
+)
+
+# Example of one evaluation object for heatwave
+heatwave_evaluation_object = [
+    inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=[
             metrics.MaximumMAE,
@@ -106,30 +98,20 @@ heatwave_metric_list = [
             metrics.DurationME,
             metrics.MaxMinMAE,
         ],
-        target_config=era5_target_config,
-        forecast_config=cira_forecast_config,
-    ),
-    # rs.MetricEvaluationObject(
-    #     event_type="heat_wave",
-    #     metric_list=[
-    #         crs.MaximumMAE,
-    #         crs.RMSE,
-    #         crs.OnsetME,
-    #         crs.DurationME,
-    #         crs.MaxMinMAE,
-    #     ],
-    #     target_config=ghcn_target_config,
-    #     forecast_config=cira_forecast_config,
-    # ),
+        target=ghcn_target,
+        forecast=hres_forecast,
+    )
 ]
 
+# Initialize ExtremeWeatherBench
 test_ewb = evaluate.ExtremeWeatherBench(
-    cases=test_yaml,
-    metrics=heatwave_metric_list,
+    cases=case_yaml,
+    evaluation_objects=heatwave_evaluation_object,
 )
-logger.info("Starting EWB run")
 
+# Get case operators
 case_operators = test_ewb.case_operators
+
 # Get the number of available CPUs for determining n_processes
 n_threads_per_process = 10
 n_processes = max(1, multiprocessing.cpu_count() // n_threads_per_process)
@@ -140,11 +122,10 @@ os.environ["MKL_NUM_THREADS"] = str(n_threads_per_process)
 os.environ["OPENBLAS_NUM_THREADS"] = str(n_threads_per_process)
 os.environ["NUMEXPR_NUM_THREADS"] = str(n_threads_per_process)
 
-joblib_config = joblib.parallel_config(n_jobs=n_processes, prefer="processes")
 # Use joblib to parallelize with n processes, each with 10 threads
 with logging_redirect_tqdm():
     joblib.Parallel(n_jobs=n_processes, prefer="processes")(
-        joblib.delayed(test_ewb.compute_case_operator)(
+        joblib.delayed(evaluate.compute_case_operator)(
             case_op, tolerance_range=48, pre_compute=True
         )
         for case_op in tqdm(case_operators)
