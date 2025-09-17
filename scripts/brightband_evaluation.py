9cea5594e9c2988d31266bd2041053b51a717a57
taylor <mandelbaum.taylor@gmail.com>
2025-09-17 14:35:47 +0000

    manage warnings in bb eval

--- scripts/brightband_evaluation.py
+++ scripts/brightband_evaluation.py
@@ -1,7 +1,7 @@
 import logging
 import multiprocessing
+import warnings
 
-import pandas as pd
 from tqdm.contrib.logging import logging_redirect_tqdm
 
 from extremeweatherbench import cases, defaults, evaluate
@@ -9,11 +9,18 @@ from extremeweatherbench import cases, defaults, evaluate
 # Suppress noisy log messages
 logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
 logging.getLogger("botocore.httpchecksum").setLevel(logging.CRITICAL)
+
+
+logging.getLogger("joblib").setLevel(logging.CRITICAL)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Suppress joblib/loky warnings about worker timeouts and child process errors
+warnings.filterwarnings("ignore", category=UserWarning, module="joblib")
+
 # Load events yaml
 case_yaml = cases.load_ewb_events_yaml_into_case_collection()
+case_yaml.cases = case_yaml.cases[95:]
 # Initialize ExtremeWeatherBench
 ewb = evaluate.ExtremeWeatherBench(
     cases=case_yaml,
@@ -24,6 +31,4 @@ n_threads_per_process = 10
 n_processes = max(1, multiprocessing.cpu_count() // n_threads_per_process)
 with logging_redirect_tqdm(loggers=[logger]):
     results = ewb.run(parallel=True, n_jobs=n_processes, pre_compute=True)
-
-results = pd.concat(results, ignore_index=True)
 results.to_csv("brightband_evaluation_results.csv", index=False)
