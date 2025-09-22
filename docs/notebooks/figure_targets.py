a371c0de596eabfcde20341902bcca54689dfac3
aaTman <mandelbaum.taylor@gmail.com>
2025-09-22 19:29:35 +0000

    added figure_targets script with accessible list

---
+++ docs/notebooks/figure_targets.py
@@ -0,0 +1,200 @@
+import logging
+
+import numpy as np
+import xarray as xr
+
+from extremeweatherbench import inputs, metrics
+
+# Suppress noisy log messages
+logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
+logging.getLogger("botocore.httpchecksum").setLevel(logging.CRITICAL)
+
+
+def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
+    """An example preprocess function that renames the time coordinate to lead_time,
+    creates a valid_time coordinate, and sets the lead time range and resolution not
+    present in the original dataset.
+
+    Args:
+        ds: The forecast dataset to rename.
+
+    Returns:
+        The renamed forecast dataset.
+    """
+    ds = ds.rename({"time": "lead_time"})
+    # The evaluation configuration is used to set the lead time range and resolution.
+    ds["lead_time"] = np.array(
+        [i for i in range(0, 241, 6)], dtype="timedelta64[h]"
+    ).astype("timedelta64[ns]")
+    return ds
+
+
+# GHCN targets
+ghcn_heatwave_target = inputs.GHCN(
+    source=inputs.DEFAULT_GHCN_URI,
+    variables=["surface_air_temperature"],
+    variable_mapping={"t2": "surface_air_temperature"},
+    storage_options={},
+)
+
+ghcn_freeze_target = inputs.GHCN(
+    source=inputs.DEFAULT_GHCN_URI,
+    variables=[
+        "surface_air_temperature",
+        "surface_eastward_wind",
+        "surface_northward_wind",
+    ],
+    variable_mapping={
+        "surface_temperature": "surface_air_temperature",
+        "surface_eastward_wind": "surface_eastward_wind",
+        "surface_northward_wind": "surface_northward_wind",
+    },
+    storage_options={},
+)
+
+# LSR and PPH targets
+lsr_target = inputs.LSR(
+    source=inputs.LSR_URI,
+    variables=["local_storm_reports"],
+    variable_mapping={},
+    storage_options={"remote_options": {"anon": True}},
+)
+
+pph_target = inputs.PPH(
+    source=inputs.PPH_URI,
+    variables=["practically_perfect_hindcast"],
+    variable_mapping={},
+    storage_options={"remote_options": {"anon": True}},
+)
+
+# IBTrACS target
+ibtracs_target = inputs.IBTrACS(
+    source=inputs.IBTRACS_URI,
+    variables=[
+        "surface_wind_speed",
+        "air_pressure_at_mean_sea_level",
+    ],
+    variable_mapping=inputs.IBTrACS_metadata_variable_mapping,
+    storage_options={},
+)
+
+# Forecast Examples
+
+cira_heatwave_forecast = inputs.KerchunkForecast(
+    source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
+    variables=["surface_air_temperature"],
+    variable_mapping={"t2": "surface_air_temperature"},
+    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
+    preprocess=_preprocess_bb_cira_forecast_dataset,
+)
+
+cira_freeze_forecast = inputs.KerchunkForecast(
+    source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
+    variables=[
+        "surface_air_temperature",
+        "surface_eastward_wind",
+        "surface_northward_wind",
+    ],
+    variable_mapping={
+        "t2": "surface_air_temperature",
+        "10u": "surface_eastward_wind",
+        "10v": "surface_northward_wind",
+    },
+    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
+    preprocess=_preprocess_bb_cira_forecast_dataset,
+)
+
+# Severe convection forecast
+cira_severe_convection_forecast = inputs.KerchunkForecast(
+    source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
+    variables=[],
+    variable_mapping={
+        "t": "air_temperature",
+        "t2": "surface_air_temperature",
+        "z": "geopotential",
+        "r": "relative_humidity",
+        "u": "eastward_wind",
+        "v": "northward_wind",
+        "10u": "surface_eastward_wind",
+        "10v": "surface_northward_wind",
+    },
+    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
+)
+
+cira_tropical_cyclone_forecast = inputs.KerchunkForecast(
+    source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
+    variables=[
+        "surface_wind_speed",
+        "air_pressure_at_mean_sea_level",
+    ],
+    variable_mapping={
+        "t": "air_temperature",
+        "t2": "surface_air_temperature",
+        "z": "geopotential",
+        "r": "relative_humidity",
+        "u": "eastward_wind",
+        "v": "northward_wind",
+        "10u": "surface_eastward_wind",
+        "10v": "surface_northward_wind",
+    },
+    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
+)
+
+FIGURE_TARGETS_EVALUATION_OBJECTS = [
+    inputs.EvaluationObject(
+        event_type="heat_wave",
+        metric_list=[
+            metrics.MaximumMAE,
+            metrics.RMSE,
+            metrics.OnsetME,
+            metrics.DurationME,
+            metrics.MaxMinMAE,
+        ],
+        target=ghcn_heatwave_target,
+        forecast=cira_heatwave_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="freeze",
+        metric_list=[
+            metrics.MinimumMAE,
+            metrics.RMSE,
+            metrics.OnsetME,
+            metrics.DurationME,
+        ],
+        target=ghcn_freeze_target,
+        forecast=cira_freeze_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="severe_convection",
+        metric_list=[
+            metrics.CSI,
+            metrics.FAR,
+            metrics.RegionalHitsMisses,
+            metrics.HitsMisses,
+        ],
+        target=lsr_target,
+        forecast=cira_severe_convection_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="severe_convection",
+        metric_list=[
+            metrics.CSI,
+            metrics.FAR,
+            metrics.RegionalHitsMisses,
+            metrics.HitsMisses,
+        ],
+        target=pph_target,
+        forecast=cira_severe_convection_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="tropical_cyclone",
+        metric_list=[
+            metrics.EarlySignal,
+            metrics.LandfallDisplacement,
+            metrics.LandfallTimeME,
+            metrics.LandfallIntensityMAE,
+        ],
+        target=ibtracs_target,
+        forecast=cira_tropical_cyclone_forecast,
+    ),
+]
