b26735b3e84d6db0deb1431ca5bcb36811c25c3a
taylor <mandelbaum.taylor@gmail.com>
2025-10-03 20:21:55 +0000

    add valid time funcs to init

--- src/extremeweatherbench/sources/__init__.py
+++ src/extremeweatherbench/sources/__init__.py
@@ -1,13 +1,29 @@
 """Input source modules for different data types."""
 
-from .pandas_dataframe import safely_pull_variables_pandas_dataframe
-from .polars_lazyframe import safely_pull_variables_polars_lazyframe
-from .xarray_dataarray import safely_pull_variables_xr_dataarray
-from .xarray_dataset import safely_pull_variables_xr_dataset
+from .pandas_dataframe import (
+    check_for_valid_times_pandas_dataframe,
+    safely_pull_variables_pandas_dataframe,
+)
+from .polars_lazyframe import (
+    check_for_valid_times_polars_lazyframe,
+    safely_pull_variables_polars_lazyframe,
+)
+from .xarray_dataarray import (
+    check_for_valid_times_xr_dataarray,
+    safely_pull_variables_xr_dataarray,
+)
+from .xarray_dataset import (
+    check_for_valid_times_xr_dataset,
+    safely_pull_variables_xr_dataset,
+)
 
 __all__ = [
     "safely_pull_variables_xr_dataset",
     "safely_pull_variables_xr_dataarray",
     "safely_pull_variables_polars_lazyframe",
     "safely_pull_variables_pandas_dataframe",
+    "check_for_valid_times_xr_dataset",
+    "check_for_valid_times_xr_dataarray",
+    "check_for_valid_times_polars_lazyframe",
+    "check_for_valid_times_pandas_dataframe",
 ]
