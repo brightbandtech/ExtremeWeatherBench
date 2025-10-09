afd358d5a3e16667af2b61894b20ade6a46656d6
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2025-10-09 12:12:42 -0400

    Update applied scripts (#176)

--- docs/examples/applied_heatwave.py
+++ docs/examples/applied_heatwave.py
@@ -1,106 +1,75 @@
 import logging
 
-import numpy as np
-import xarray as xr
+from extremeweatherbench import cases, evaluate, inputs, metrics
 
-from extremeweatherbench import evaluate, inputs, metrics, utils
+# Set the logger level to INFO
+logger = logging.getLogger("extremeweatherbench")
+logger.setLevel(logging.INFO)
 
-logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
-logging.getLogger("botocore.httpchecksum").setLevel(logging.CRITICAL)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-case_yaml = utils.load_events_yaml()
-test_yaml = {"cases": case_yaml["cases"][:1]}
+# Load case data from the default events.yaml
+# Users can also define their own cases_dict structure
+case_yaml = cases.load_ewb_events_yaml_into_case_collection()
 
 
-def _preprocess_bb_cira_forecast_dataset(ds: xr.Dataset) -> xr.Dataset:
-    """An example preprocess function that renames the time coordinate to lead_time,
-    creates a valid_time coordinate, and sets the lead time range and resolution not
-    present in the original dataset.
-
-    Args:
-        ds: The forecast dataset to rename.
-
-    Returns:
-        The renamed forecast dataset.
-    """
-    ds = ds.rename({"time": "lead_time"})
-    # The evaluation configuration is used to set the lead time range and resolution.
-    ds["lead_time"] = np.array(
-        [i for i in range(0, 241, 6)], dtype="timedelta64[h]"
-    ).astype("timedelta64[ns]")
-    return ds
-
-
+# Define targets
+# ERA5 target
 era5_heatwave_target = inputs.ERA5(
-    source=inputs.ARCO_ERA5_FULL_URI,
     variables=["surface_air_temperature"],
-    variable_mapping={
-        "2m_temperature": "surface_air_temperature",
-        "time": "valid_time",
-    },
-    storage_options={"remote_options": {"anon": True}},
     chunks=None,
 )
 
+# GHCN target
 ghcn_target = inputs.GHCN(
-    source=inputs.DEFAULT_GHCN_URI,
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
-    storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
 )
 
+# Define forecast (HRES)
 hres_forecast = inputs.ZarrForecast(
+    name="hres_forecast",
     source="gs://weatherbench2/datasets/hres/2016-2022-0012-1440x721.zarr",
     variables=["surface_air_temperature"],
-    variable_mapping={
-        "2m_temperature": "surface_air_temperature",
-        "prediction_timedelta": "lead_time",
-        "time": "init_time",
-    },
-    storage_options={"remote_options": {"anon": True}},
+    variable_mapping=inputs.HRES_metadata_variable_mapping,
 )
 
-# just one for now
-heatwave_metric_list = [
+metrics_list = [
+    metrics.MaximumMAE,
+    metrics.RMSE,
+    metrics.OnsetME,
+    metrics.DurationME,
+    metrics.MaxMinMAE,
+]
+# Create a list of evaluation objects for heatwave
+heatwave_evaluation_object = [
     inputs.EvaluationObject(
         event_type="heat_wave",
-        metric_list=[
-            metrics.MaximumMAE,
-            metrics.RMSE,
-            metrics.OnsetME,
-            metrics.DurationME,
-            metrics.MaxMinMAE,
-        ],
+        metric_list=metrics_list,
+        target=ghcn_target,
+        forecast=hres_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="heat_wave",
+        metric_list=metrics_list,
         target=era5_heatwave_target,
         forecast=hres_forecast,
     ),
-    # rs.EvaluationObject(
-    #     event_type="heat_wave",
-    #     metric_list=[
-    #         crs.MaximumMAE,
-    #         crs.RMSE,
-    #         crs.OnsetME,
-    #         crs.DurationME,
-    #         crs.MaxMinMAE,
-    #     ],
-    #     target_config=ghcn_target_config,
-    #     forecast_config=cira_forecast_config,
-    # ),
 ]
 
-test_ewb = evaluate.ExtremeWeatherBench(
-    cases=test_yaml,
-    metrics=heatwave_metric_list,
+# Initialize ExtremeWeatherBench
+ewb = evaluate.ExtremeWeatherBench(
+    cases=case_yaml,
+    evaluation_objects=heatwave_evaluation_object,
 )
-logger.info("Starting EWB run")
-outputs = test_ewb.run(
+
+# Run the workflow
+outputs = ewb.run(
     # tolerance range is the number of hours before and after the timestamp a
-    # validating occurrence is checked in the forecasts
+    # validating occurrence is checked in the forecasts for certain metrics
+    # such as minimum temperature MAE
     tolerance_range=48,
-    # pre-compute the datasets to avoid recomputing them for each metric
+    # precompute the datasets before metrics are calculated, to avoid IO costs loading
+    # them into memory for each metric
     pre_compute=True,
 )
 
-print(outputs.head())
+# Print the outputs; can be saved if desired
+logger.info(outputs.head())
