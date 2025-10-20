c74e0d3af2912eb6b6db87eecc2598d633073a7d
taylor <mandelbaum.taylor@gmail.com>
2025-10-20 15:50:07 +0000

    update lsr link

--- src/extremeweatherbench/inputs.py
+++ src/extremeweatherbench/inputs.py
@@ -24,7 +24,7 @@ ARCO_ERA5_FULL_URI = (
 DEFAULT_GHCN_URI = "gs://extremeweatherbench/datasets/ghcnh_all_2020_2024.parq"
 
 #: Storage/access options for local storm report (LSR) tabular data.
-LSR_URI = "gs://extremeweatherbench/datasets/lsr_01012020_04302025.parq"
+LSR_URI = "gs://extremeweatherbench/datasets/combined_canada_australia_us_lsr_01012020_09272025.parq"  # noqa: E501
 
 PPH_URI = (
     "gs://extremeweatherbench/datasets/"
