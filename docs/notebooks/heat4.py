fafed3935268856f69223755ee8b57ec9d9bf3c0
taylor <mandelbaum.taylor@gmail.com>
2025-11-23 02:38:04 +0000

    swap to loky, formatting, ignore sys import linter issue,

--- docs/notebooks/heat4.py
+++ docs/notebooks/heat4.py
@@ -1,29 +1,21 @@
 # setup all the imports
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+from pathlib import Path
+
 import pandas as pd
 import seaborn as sns
-from extremeweatherbench import evaluate, utils, cases, defaults, inputs, metrics, regions
-sns.set_theme(style='whitegrid')
-import shapely
-from pathlib import Path
-import multiprocessing
-import xarray as xr
-from matplotlib.lines import Line2D
-import matplotlib.colors as mcolors
-from collections import namedtuple
+
+from extremeweatherbench import cases, defaults, evaluate, inputs, metrics
+
+sns.set_theme(style="whitegrid")
 
 # make the basepath - change this to your local path
-basepath = Path.home() / 'ExtremeWeatherBench' / ''
-basepath = str(basepath) + '/'
+basepath = Path.home() / "ExtremeWeatherBench" / ""
+basepath = str(basepath) + "/"
 
 # ugly hack to load in our plotting scripts
-import sys
-sys.path.append(basepath + "/docs/notebooks/")
-import paper_plotting as pp
+import sys  # noqa: E402
 
-import operator
+sys.path.append(basepath + "/docs/notebooks/")
 
 
 # setup the templates to load in the data
@@ -32,7 +24,7 @@ import operator
 cira_heatwave_forecast_FOURv2_IFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/FOUR_v200_IFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA FOURv2 IFS",
@@ -41,7 +33,7 @@ cira_heatwave_forecast_FOURv2_IFS = inputs.KerchunkForecast(
 cira_heatwave_forecast_GC_IFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/GRAP_v100_IFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA GC IFS",
@@ -50,7 +42,7 @@ cira_heatwave_forecast_GC_IFS = inputs.KerchunkForecast(
 cira_heatwave_forecast_PANG_IFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/PANG_v100_IFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA PANG IFS",
@@ -59,7 +51,7 @@ cira_heatwave_forecast_PANG_IFS = inputs.KerchunkForecast(
 cira_heatwave_forecast_FOURv2_GFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/FOUR_v200_GFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA FOURv2 GFS",
@@ -68,7 +60,7 @@ cira_heatwave_forecast_FOURv2_GFS = inputs.KerchunkForecast(
 cira_heatwave_forecast_GC_GFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/GRAP_v100_GFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA GC GFS",
@@ -77,7 +69,7 @@ cira_heatwave_forecast_GC_GFS = inputs.KerchunkForecast(
 cira_heatwave_forecast_PANG_GFS = inputs.KerchunkForecast(
     source="gs://extremeweatherbench/PANG_v100_GFS.parq",
     variables=["surface_air_temperature"],
-    variable_mapping={"t2": "surface_air_temperature"},
+    variable_mapping=inputs.CIRA_metadata_variable_mapping,
     storage_options={"remote_protocol": "s3", "remote_options": {"anon": True}},
     preprocess=defaults._preprocess_bb_cira_forecast_dataset,
     name="CIRA PANG GFS",
@@ -92,35 +84,35 @@ hres_forecast = inputs.ZarrForecast(
 )
 
 heat_metrics = [
-            metrics.MaximumMeanAbsoluteError,
-            metrics.RootMeanSquaredError,
-            metrics.MaximumLowestMeanAbsoluteError,
-        ]
+    metrics.MaximumMeanAbsoluteError(),
+    metrics.RootMeanSquaredError(),
+    metrics.MaximumLowestMeanAbsoluteError(),
+]
 
 FOURv2_HEAT_EVALUATION_OBJECTS = [
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_FOURv2_IFS, 
+        forecast=cira_heatwave_forecast_FOURv2_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_FOURv2_GFS, 
+        forecast=cira_heatwave_forecast_FOURv2_GFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_FOURv2_IFS, 
+        forecast=cira_heatwave_forecast_FOURv2_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_FOURv2_GFS, 
+        forecast=cira_heatwave_forecast_FOURv2_GFS,
     ),
 ]
 
@@ -129,25 +121,25 @@ GC_HEAT_EVALUATION_OBJECTS = [
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_GC_IFS, 
+        forecast=cira_heatwave_forecast_GC_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_GC_GFS, 
+        forecast=cira_heatwave_forecast_GC_GFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_GC_IFS, 
+        forecast=cira_heatwave_forecast_GC_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_GC_GFS, 
+        forecast=cira_heatwave_forecast_GC_GFS,
     ),
 ]
 
@@ -156,25 +148,25 @@ PANG_HEAT_EVALUATION_OBJECTS = [
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_PANG_IFS, 
+        forecast=cira_heatwave_forecast_PANG_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.ghcn_heatwave_target,
-        forecast=cira_heatwave_forecast_PANG_GFS, 
+        forecast=cira_heatwave_forecast_PANG_GFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_PANG_IFS, 
+        forecast=cira_heatwave_forecast_PANG_IFS,
     ),
     inputs.EvaluationObject(
         event_type="heat_wave",
         metric_list=heat_metrics,
         target=defaults.era5_heatwave_target,
-        forecast=cira_heatwave_forecast_PANG_GFS, 
+        forecast=cira_heatwave_forecast_PANG_GFS,
     ),
 ]
 
@@ -195,7 +187,7 @@ HRES_HEAT_EVALUATION_OBJECTS = [
 
 # load in all of the events in the yaml file
 ewb_cases = cases.load_ewb_events_yaml_into_case_collection()
-ewb_cases = ewb_cases.select_cases('event_type', 'heat_wave')
+ewb_cases = ewb_cases.select_cases("event_type", "heat_wave")
 
 ewb_fourv2 = evaluate.ExtremeWeatherBench(ewb_cases, FOURv2_HEAT_EVALUATION_OBJECTS)
 ewb_gc = evaluate.ExtremeWeatherBench(ewb_cases, GC_HEAT_EVALUATION_OBJECTS)
@@ -206,26 +198,30 @@ ewb_hres = evaluate.ExtremeWeatherBench(ewb_cases, HRES_HEAT_EVALUATION_OBJECTS)
 # set this to true to make the results from scratch (it is slow)
 load_results = False
 
-if (load_results):
+if load_results:
     # load the results back in
-    fourv2_results = pd.read_pickle(basepath + 'docs/notebooks/figs/fourv2_heat_results.pkl')
-    pang_results = pd.read_pickle(basepath + 'docs/notebooks/figs/pang_heat_results.pkl')
-    hres_results = pd.read_pickle(basepath + 'docs/notebooks/figs/hres_heat_results.pkl')
-    gc_results = pd.read_pickle(basepath + 'docs/notebooks/figs/gc_heat_results.pkl')
+    fourv2_results = pd.read_pickle(
+        basepath + "docs/notebooks/figs/fourv2_heat_results.pkl"
+    )
+    pang_results = pd.read_pickle(
+        basepath + "docs/notebooks/figs/pang_heat_results.pkl"
+    )
+    hres_results = pd.read_pickle(
+        basepath + "docs/notebooks/figs/hres_heat_results.pkl"
+    )
+    gc_results = pd.read_pickle(basepath + "docs/notebooks/figs/gc_heat_results.pkl")
 else:
     # load in the results for all heat waves in parallel
-    # this will take awhile to run if you do them all in one code box 
+    # this will take awhile to run if you do them all in one code box
     # if you have already saved them (from running this once), then skip this box
-    n_threads_per_process = 4
-    # n_processes = max(1, multiprocessing.cpu_count() // n_threads_per_process)
 
     # fourv2_results = ewb_fourv2.run(n_jobs=1)
     # gc_results = ewb_gc.run(n_jobs=1)
-    #pang_results = ewb_pang.run(n_jobs=1)
-    hres_results = ewb_hres.run(n_jobs=1)
+    # pang_results = ewb_pang.run(n_jobs=1)
+    hres_results = ewb_hres.run(parallel_config={"backend": "loky", "n_jobs": 24})
 
     # save the results to make it more efficient
     # fourv2_results.to_pickle(basepath + 'docs/notebooks/figs/fourv2_heat_results.pkl')
     # gc_results.to_pickle(basepath + 'docs/notebooks/figs/gc_heat_results.pkl')
     # pang_results.to_pickle(basepath + 'docs/notebooks/figs/pang_heat_results.pkl')
-    hres_results.to_pickle(basepath + 'docs/notebooks/figs/hres_heat_results.pkl')
+    hres_results.to_pickle(basepath + "docs/notebooks/figs/hres_heat_results.pkl")
