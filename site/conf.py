c2967f6b3d24ee2001d00ee56dfacdf19d66a45a
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2025-12-16 20:00:13 -0500

    update directories, mkdocs, remove sphinx references

---
+++ site/conf.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath("../../src/extremeweatherbench")
+)  # Source code dir relative to this file
+
+# -- Project information
+utc_now = datetime.datetime.now(datetime.UTC).strftime("%H:%M %d %b %Y")
+project = "ExtremeWeatherBench"
+copyright = (
+    f"{datetime.datetime.now(datetime.UTC).strftime('%Y')}, "
+    "Brightband.    ♻ Updated: {utc_now}"
+)
+author = "Taylor Mandelbaum"
+
+release = "0.1"
+version = "0.1.0"
+
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "myst_parser",
+    "autodoc2",
+    "nbsphinx",
+]
+
+autodoc2_render_plugin = "myst"
+
+autodoc2_packages = [
+    "../../src/extremeweatherbench",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output
+html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
+html_css_files = ["_static/custom.css"]
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
+autosummary_generate = True
+autodoc_inherit_docstrings = True
+autodoc_typehints = "signature"
+autodoc_typehints_description_target = "documented"
+autodoc_typehints_format = "short"
+
+nb_execution_mode = "off"
---
+++ site/examples/applied_heatwave.py
@@ -0,0 +1,69 @@
+import logging
+import operator
+
+from extremeweatherbench import cases, defaults, evaluate, inputs, metrics
+
+# Set the logger level to INFO
+logger = logging.getLogger("extremeweatherbench")
+logger.setLevel(logging.INFO)
+
+# Load case data from the default events.yaml
+# Users can also define their own cases_dict structure
+case_yaml = cases.load_ewb_events_yaml_into_case_collection()
+
+# Define targets
+# ERA5 target
+era5_heatwave_target = inputs.ERA5(
+    variables=["surface_air_temperature"],
+    chunks=None,
+)
+
+# GHCN target
+ghcn_heatwave_target = inputs.GHCN(
+    variables=["surface_air_temperature"],
+)
+
+# Define forecast (HRES)
+hres_forecast = inputs.ZarrForecast(
+    name="hres_forecast",
+    source="gs://weatherbench2/datasets/hres/2016-2022-0012-1440x721.zarr",
+    variables=["surface_air_temperature"],
+    variable_mapping=inputs.HRES_metadata_variable_mapping,
+)
+
+# Load the climatology for DurationMeanError
+climatology = defaults.get_climatology(quantile=0.85)
+
+# Define the metrics
+metrics_list = [
+    metrics.MaximumMeanAbsoluteError(),
+    metrics.RootMeanSquaredError(),
+    metrics.DurationMeanError(threshold_criteria=climatology, op_func=operator.ge),
+    metrics.MaximumLowestMeanAbsoluteError(),
+]
+
+# Create a list of evaluation objects for heatwave
+heatwave_evaluation_object = [
+    inputs.EvaluationObject(
+        event_type="heat_wave",
+        metric_list=metrics_list,
+        target=ghcn_heatwave_target,
+        forecast=hres_forecast,
+    ),
+    inputs.EvaluationObject(
+        event_type="heat_wave",
+        metric_list=metrics_list,
+        target=era5_heatwave_target,
+        forecast=hres_forecast,
+    ),
+]
+if __name__ == "__main__":
+    # Initialize ExtremeWeatherBench
+    ewb = evaluate.ExtremeWeatherBench(
+        case_metadata=case_yaml,
+        evaluation_objects=heatwave_evaluation_object,
+    )
+
+    # Run the workflow
+    outputs = ewb.run(parallel_config={"backend": "loky", "n_jobs": 2})
+    outputs.to_csv("applied_heatwave_outputs.csv")
